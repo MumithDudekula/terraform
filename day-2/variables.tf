f8517791d2c310cde90794583a3328830e06abc3
Mumith <mumithdudekula@gmail.com>
2024-03-16 17:35:41 +0530

    day2 update

---
+++ day-2/variables.tf
@@ -0,0 +1,14 @@
+variable "ami_id" {
+  description = "ami id"
+  type = string
+}
+
+variable "instance_type" {
+  description = "instance type"
+  type = string
+}
+
+variable "key_name" {
+  description = "key name"
+  type = string
+}
