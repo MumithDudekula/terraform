bf189f0f54890af3432490617fd385967625d813
Mumith <mumithdudekula@gmail.com>
2024-03-21 12:04:21 +0530

    day9 module1 commit

---
+++ day-9-modules/chaild.tf
@@ -0,0 +1,9 @@
+
+module "test" {
+    source = "./module" 
+    ami       	= "ami-02d7fd1c2af6eead0"
+    instance_type = "t2.micro"
+    subnet_id 	= "	subnet-0cdc37180788948e1"
+    key_name  	= "nit"
+    name        = "test_ec2"
+}
