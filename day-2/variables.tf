9a9d6bbc47ee7bd085d33747722e40a726a4d8d1
MumithDudekula <162279828+MumithDudekula@users.noreply.github.com>
2024-03-26 12:32:45 +0530

    Update variables.tf

--- day-2/variables.tf
+++ day-2/variables.tf
@@ -1,14 +1,17 @@
 variable "ami_id" {
   description = "ami id"
   type = string
+  default = "ami-033a1ebf088e56e81"
 }
 
 variable "instance_type" {
   description = "instance type"
   type = string
+  default = "t2.micro"
 }
 
 variable "key_name" {
   description = "key name"
   type = string
+  default = "nit"
 }
